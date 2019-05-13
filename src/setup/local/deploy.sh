39f836b41803076ee1ec71c1ea1263a9e837347c
Tomas Gramblicka <tomas.gramblicka@cargo-partner.com>
2019-05-13 16:08:38 +0200

    deploy scripts for local added

--- src/setup/local/deploy.sh
+++ src/setup/local/deploy.sh
@@ -2,6 +2,38 @@
 
 payara=/e/payara/
 #payara=/c/Users/tomas/dev
-asadmin=${payara}/payara5/glassfish/bin/asadmin
+project_location="/e/workspace_idea/trnka/trnka-backend"
+war_name="trnka-backend.war"
 
-${asadmin} start-domain domain1
+
+
+payara_location=${payara}/payara5/glassfish
+asadmin=${payara}/payara5/glassfish/bin/asadmin
+asadminpass="$asadmin --user admin --passwordfile=$project_location/src/setup/local/asadmin_password_file"
+domain="domain1"
+
+
+now=$(date +"%T")
+echo $now
+
+${asadmin} stop-domain ${domain}
+rm -rf ${payara_location}/domains/${domain}/applications/*
+rm -rf ${payara_location}/domains/${domain}/generated/*
+rm -rf ${payara_location}/domains/${domain}/osgi-cache/*
+rm -rf ${payara_location}/domains/${domain}/eclipseApps/*
+rm -rf ${payara_location}/domains/${domain}/eclipseAppsTmp/*
+
+> ${payara_location}/domains/${domain}/logs/server.log
+
+
+${asadmin} start-domain --debug ${domain}
+${asadminpass} deploy --force ${project_location}/target/${war_name}
+
+
+#cp ${project_location}/setup/local/domain.xml ${payara_location}/domains/${domain}/config/domain.xml
+
+
+
+#${asadmin} start-domain domain1
+#asadmin undeploy trnka-backend
+
---
+++ src/setup/wildfly/deploy.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+wildfly_location=/e/wildfly/wildfly-16.0.0.Final/bin
+standalone=${wildfly_location}/standalone.sh
+
+${standalone} &
+
