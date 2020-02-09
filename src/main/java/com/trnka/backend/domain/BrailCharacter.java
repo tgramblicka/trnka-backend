152a5e1d3e1ed491b3c5dc304403ec5d2581b90e
Tomas Gramblicka <t.gramblicka@gmail.com>
2020-02-09 23:13:04 +0100

    brailCharacter letter is unique

--- src/main/java/com/trnka/backend/domain/BrailCharacter.java
+++ src/main/java/com/trnka/backend/domain/BrailCharacter.java
@@ -8,14 +8,15 @@ import javax.persistence.FetchType;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToOne;
 
-import com.vladmihalcea.hibernate.type.json.JsonStringType;
 import org.hibernate.annotations.Type;
+import org.hibernate.annotations.TypeDef;
+
+import com.vladmihalcea.hibernate.type.json.JsonStringType;
 
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.hibernate.annotations.TypeDef;
 
 @Getter
 @Setter
@@ -25,8 +26,8 @@ import org.hibernate.annotations.TypeDef;
 @TypeDef(name = "json", typeClass = JsonStringType.class)
 public class BrailCharacter extends BaseEntity {
 
+    @Column(unique = true, name = "letter")
     private String letter;
-
 
     @Column(name = "brail_representation", columnDefinition = "json")
     @Type(type = "json")
