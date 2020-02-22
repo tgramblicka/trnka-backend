007892e56485cd46e8edfb7748956f03a097dbc6
Tomas Gramblicka <t.gramblicka@gmail.com>
2020-02-22 18:57:57 +0100

    removed dead classes + sync service

---
+++ src/main/java/com/trnka/backend/service/sync/SyncService.java
@@ -0,0 +1,10 @@
+package com.trnka.backend.service.sync;
+
+import com.trnka.restapi.dto.SyncDto;
+
+public class SyncService {
+
+    public SyncDto sync() {
+        return new SyncDto();
+    }
+}
