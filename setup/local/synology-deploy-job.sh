398dff4e48f1fe645306f85a56bd0403f8c9fefd
Tomas Gramblicka <t.gramblicka@gmail.com>
2020-02-23 22:14:05 +0100

    upgraded checker script with exporting env variables (cause it runs from crontab)

--- setup/local/synology-deploy-job.sh
+++ setup/local/synology-deploy-job.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
+
 if [ $(curl -LI localhost:8080/vst/monitoring/alive -o /dev/null -w '%{http_code}\n' -s) != "200" ]; then
+
+        # trnka development
+        MARIA_DB_USER=admin
+        export MARIA_DB_USER
+
+        MARIA_DB_PWD=VST-user-123
+        export MARIA_DB_PWD
+
+        MARIA_DB_DATASOURCE_URL='jdbc:mariadb://localhost:3307/vst?autoReconnect=true&relaxAutoCommit=true'
+        export MARIA_DB_DATASOURCE_URL
+
         echo "not deployed, will run deployment";
         logFile="/volume1/homes/tomas/dev/trnka/vst/log-`date '+%Y-%m-%d-%H:%M:%S'`.log"
         touch $logFile;
