d5c3a1408285138c056e9c1aa6a518b264146895
Tomas Gramblicka <t.gramblicka@gmail.com>
2021-01-06 19:14:33 +0100

    Adapted to new sync API in restApi

--- src/main/java/com/trnka/backend/controller/StudentSyncController.java
+++ src/main/java/com/trnka/backend/controller/StudentSyncController.java
@@ -3,14 +3,15 @@ package com.trnka.backend.controller;
 import com.trnka.backend.service.sync.StudentSyncService;
 import com.trnka.restapi.dto.StudentDTO;
 import com.trnka.restapi.dto.SyncDto;
-import com.trnka.restapi.endpoint.StudentSyncEndpoint;
+import com.trnka.restapi.dto.statistics.DeviceStatisticsSyncDto;
+import com.trnka.restapi.endpoint.SyncEndpoint;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @Slf4j
-public class StudentSyncController implements StudentSyncEndpoint {
+public class StudentSyncController implements SyncEndpoint {
 
     @Autowired
     private StudentSyncService studentSyncService;
@@ -24,5 +25,10 @@ public class StudentSyncController implements StudentSyncEndpoint {
     public SyncDto syncAll() {
         return studentSyncService.getSyncDto();
     }
-}
 
+    @Override
+    public Void updateExaminationStatisticsToAllStudents(final DeviceStatisticsSyncDto deviceStatisticsSyncDto) {
+        // todo implement
+        return null;
+    }
+}
