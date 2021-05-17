d7e10ca33b32aee5e53b57439e17768703d36c1f
Tomas Gramblicka <t.gramblicka@gmail.com>
2021-05-17 22:34:57 +0200

    create index deleted

--- src/main/resources/db/changelog/changes/000-create-schema.sql
+++ src/main/resources/db/changelog/changes/000-create-schema.sql
@@ -18,19 +18,12 @@ create sequence vst_seq start with 100 minvalue 100 maxvalue 9223372036854775806
             nocycle;
 
 
-CREATE INDEX FK1bsjbolbkeidctoruif7i3b6k ON brail_character(audio_id);
-CREATE INDEX FK38jgjyongm0ve81vkf0gnvmku ON examination_step(brail_character_id);
-CREATE INDEX FKpb6g6pahj1mr2ijg92r7m1xlh ON teacher(user_id);
-CREATE INDEX FKrg7nxjlvm7ak0gvuhwgr96v21 ON examination_step(examination_id);
-CREATE INDEX FKsybhlxoejr4j3teomm5u2bx1n ON course(teacher_id);
-CREATE INDEX FK_examination_course_id ON examination(course_id);
-CREATE INDEX FK_student_course_id ON student(course_id);
 
-ALTER TABLE brail_character ADD CONSTRAINT FK1bsjbolbkeidctoruif7i3b6k FOREIGN KEY (audio_id) REFERENCES audio (id) ON UPDATE RESTRICT ON DELETE RESTRICT;
-ALTER TABLE examination_step ADD CONSTRAINT FK38jgjyongm0ve81vkf0gnvmku FOREIGN KEY (brail_character_id) REFERENCES brail_character (id) ON UPDATE RESTRICT ON DELETE RESTRICT;
-ALTER TABLE teacher ADD CONSTRAINT FKpb6g6pahj1mr2ijg92r7m1xlh FOREIGN KEY (user_id) REFERENCES user (id) ON UPDATE RESTRICT ON DELETE RESTRICT;
-ALTER TABLE examination_step ADD CONSTRAINT FKrg7nxjlvm7ak0gvuhwgr96v21 FOREIGN KEY (examination_id) REFERENCES examination (id) ON UPDATE RESTRICT ON DELETE RESTRICT;
-ALTER TABLE course ADD CONSTRAINT FKsybhlxoejr4j3teomm5u2bx1n FOREIGN KEY (teacher_id) REFERENCES teacher (id) ON UPDATE RESTRICT ON DELETE RESTRICT;
+ALTER TABLE brail_character ADD CONSTRAINT FK_brail_character_audio_id FOREIGN KEY (audio_id) REFERENCES audio (id) ON UPDATE RESTRICT ON DELETE RESTRICT;
+ALTER TABLE examination_step ADD CONSTRAINT FK_examination_step_brail_character_id FOREIGN KEY (brail_character_id) REFERENCES brail_character (id) ON UPDATE RESTRICT ON DELETE RESTRICT;
+ALTER TABLE teacher ADD CONSTRAINT FK_teacher_user_id FOREIGN KEY (user_id) REFERENCES user (id) ON UPDATE RESTRICT ON DELETE RESTRICT;
+ALTER TABLE examination_step ADD CONSTRAINT FK_examination_step_examination_id FOREIGN KEY (examination_id) REFERENCES examination (id) ON UPDATE RESTRICT ON DELETE RESTRICT;
+ALTER TABLE course ADD CONSTRAINT FK_course_teacher_id FOREIGN KEY (teacher_id) REFERENCES teacher (id) ON UPDATE RESTRICT ON DELETE RESTRICT;
 ALTER TABLE examination ADD CONSTRAINT FK_examination_course_id FOREIGN KEY (course_id) REFERENCES course (id) ON UPDATE RESTRICT ON DELETE RESTRICT;
 ALTER TABLE student ADD CONSTRAINT FK_student_course_id FOREIGN KEY (course_id) REFERENCES course (id) ON UPDATE RESTRICT ON DELETE RESTRICT;
 
