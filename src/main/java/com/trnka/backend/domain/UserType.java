6feedd980b51468b7271396eaee5c90ba95f9002
marianni <m.rachelova@gmail.com>
2021-05-17 21:25:20 +0200

    Diakritika, dizajn, novy slovnik brailovho pisma

--- src/main/java/com/trnka/backend/domain/UserType.java
+++ src/main/java/com/trnka/backend/domain/UserType.java
@@ -1,6 +1,16 @@
 package com.trnka.backend.domain;
 
 public enum UserType {
-    ADMIN,
-    TEACHER
+    ADMIN("administrátor"),
+    TEACHER("učiteľ");
+
+    private final String translation;
+
+    UserType(String translation) {
+        this.translation = translation;
+    }
+
+    public String getTranslation() {
+        return translation;
+    }
 }
