2e8835e0e9ed00d396d452b8568d7e7061e5b463
Tomas Gramblicka <t.gramblicka@gmail.com>
2021-05-30 10:28:10 +0200

    Security config - protecting all the enpdoints with roles

--- src/main/java/com/trnka/backend/config/SecurityConfig.java
+++ src/main/java/com/trnka/backend/config/SecurityConfig.java
@@ -1,11 +1,15 @@
 package com.trnka.backend.config;
 
-import com.trnka.backend.dto.UserRole;
+import javax.sql.DataSource;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import com.trnka.backend.controller.LoginController;
 import com.trnka.backend.controller.RestApiPaths;
@@ -13,17 +17,26 @@ import com.trnka.backend.controller.RestApiPaths;
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    private DataSource dataSource;
+
     @Override
     protected void configure(final HttpSecurity http) throws Exception {
         configureWrapper(http);
     }
 
     private void configureWrapper(final HttpSecurity http) throws Exception {
-        http.csrf()
+        http.httpBasic()
+                .and()
+                .csrf()
                 .disable()
                 .authorizeRequests()
-                .antMatchers("/vst/ui/teacher/**", "/vst/ui/admin/**")
-                .hasAnyRole("TEACHER", "ADMIN")
+                .antMatchers("/vst/ui/teacher/**", "/vst/ui/course/**", "/vst/ui/examination/**", "/vst/ui/student/**", "/vst/ui/sync-management/**")
+                .hasAnyAuthority("TEACHER", "ADMIN")
+                .antMatchers("/vst/ui/admin/**", "/vst/sync/**")
+                .hasAnyAuthority("ADMIN")
+                .anyRequest()
+                .permitAll()
                 .and()
                 .formLogin()
                 .loginPage(RestApiPaths.PATH_UI + LoginController.LOGIN)
@@ -34,11 +47,29 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .logoutSuccessUrl(RestApiPaths.PATH_UI + LoginController.LOGOUT);
     }
 
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    public static void main(String[] args) {
+        BCryptPasswordEncoder code = new BCryptPasswordEncoder();
+        System.out.println("PWD:" + code.encode("123456"));
+    }
+
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication().withUser("a").password("{noop}a").roles(UserRole.TEACHER.name());
-        auth.inMemoryAuthentication().withUser("user").password("{noop}0000").roles(UserRole.TEACHER.name());
-        auth.inMemoryAuthentication().withUser("admin").password("{noop}admin").roles(UserRole.ADMIN.name());
+        // auth.inMemoryAuthentication().withUser("a").password("{noop}a").roles(UserRole.TEACHER.name());
+        // auth.inMemoryAuthentication().withUser("user").password("{noop}0000").roles(UserRole.TEACHER.name());
+        // auth.inMemoryAuthentication().withUser("admin").password("{noop}admin").roles(UserRole.ADMIN.name());
+
+        auth.jdbcAuthentication()
+                .dataSource(dataSource)
+                .passwordEncoder(passwordEncoder())
+                .usersByUsernameQuery("SELECT username, password, enabled from user where username = ?")
+                .authoritiesByUsernameQuery("SELECT u.username, a.authority " + //
+                        "FROM authorities a, user u " + //
+                        "WHERE u.username = ? AND u.id = a.user_id");
     }
 
 }
